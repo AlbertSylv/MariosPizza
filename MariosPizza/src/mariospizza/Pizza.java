baf6befe7b621177337039586bbf78c8e71753d4
AlbertSylv <basketasl@hotmail.com>
2019-10-23 15:06:11 +0200

    Albert commit

---
+++ MariosPizza/src/mariospizza/Pizza.java
@@ -0,0 +1,14 @@
+
+package mariospizza;
+
+public class Pizza {
+    int price;
+    int pizName;
+
+    public Pizza(int price, int pizName) {
+        this.price = price;
+        this.pizName = pizName;
+    }
+    //indsæt metode som henter pris og navn fra menuen. Vi laver menuen om til en arraylist hvor arrayplads[0] er prisen og arrayplads[1]
+}
+
