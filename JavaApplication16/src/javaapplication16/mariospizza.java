bd5a0237b57cd0ea06b6d25a2c829cd827ebe452
Daniel <Daniel.bisgaard@hotmail.com>
2019-10-23 13:58:05 +0200

    4rd commit

--- JavaApplication16/src/javaapplication16/mariospizza.java
+++ JavaApplication16/src/javaapplication16/mariospizza.java
@@ -6,6 +6,7 @@ public class mariospizza {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("HalliHalløj");
+        System.out.println("Virker det mon nu?");
     }
     
 }
