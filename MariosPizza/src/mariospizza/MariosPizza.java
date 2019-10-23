e2cd4550cc6bbbf16e5f683ed2c51ef6c61cfb81
AlbertSylv <basketasl@hotmail.com>
2019-10-23 13:42:18 +0200

    Albert commit

--- MariosPizza/src/mariospizza/MariosPizza.java
+++ MariosPizza/src/mariospizza/MariosPizza.java
@@ -5,7 +5,8 @@ package mariospizza;
 public class MariosPizza {
 
     public static void main(String[] args) {
-        System.out.println("daniel er luder jaaaah");
+        System.out.println("daniel er luder jaaaaah"); 
+        
     }
     
 }
