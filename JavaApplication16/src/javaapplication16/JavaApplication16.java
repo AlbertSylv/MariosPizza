71d96d94661404f88c10621e0c057e11857e992e
Daniel <Daniel.bisgaard@hotmail.com>
2019-10-23 13:29:59 +0200

    First 2nd commit

---
+++ JavaApplication16/src/javaapplication16/JavaApplication16.java
@@ -0,0 +1,11 @@
+
+package javaapplication16;
+
+public class JavaApplication16 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Albert lugter rigtig meget af tis");
+    }
+    
+}
