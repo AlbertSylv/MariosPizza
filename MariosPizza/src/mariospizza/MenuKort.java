e84a47d3cd9c649693f3c4c0eba77e31a8930485
Daniel <Daniel.bisgaard@hotmail.com>
2019-10-24 15:40:45 +0200

    Pizza liste + author

--- MariosPizza/src/mariospizza/MenuKort.java
+++ MariosPizza/src/mariospizza/MenuKort.java
@@ -1,15 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mariospizza;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
- *
- * @author danie
+ * @author Daniel
  */
 public class MenuKort {
     //TODO: Lav ting som henter pizzaer fra Pizzaer.csv således at de "kan læses på skærmen hele tiden"
-    
+    public static void MenuKort(String [] args) throws FileNotFoundException{
+        String fileName = "All Files	C:\\Users\\danie\\OneDrive\\Dokumenter\\NetBeansProjects\\MariosPizza\\MariosPizza\\src\\mariospizza\\Pizzaer.csv";
+        
+        File pizzaFil = new File(fileName);
+        
+        Scanner in = new Scanner(pizzaFil);
+        
+        int value = in.nextInt();
+        System.out.println("Read value: "+ value);
+        
+        while(in.hasNextLine()){
+            String line = in.nextLine();
+            System.out.println(line);
+        }
+        
+        in.close();
+        
+    }
 }
