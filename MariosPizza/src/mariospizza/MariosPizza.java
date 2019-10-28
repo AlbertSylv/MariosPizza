18ece24387d3a043b8bbafb2ee71250ae46fc33c
AlbertSylv <basketasl@hotmail.com>
2019-10-28 10:07:07 +0100

    Albert commit

--- MariosPizza/src/mariospizza/MariosPizza.java
+++ MariosPizza/src/mariospizza/MariosPizza.java
@@ -13,7 +13,6 @@ import java.util.Scanner;
 
 public class MariosPizza{
     
-
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(System.in);
         String s = "";
