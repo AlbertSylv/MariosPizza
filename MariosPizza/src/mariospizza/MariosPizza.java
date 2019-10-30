6450f7fd5af8b9234f4aa0330134b32e35b5baeb
AlbertSylv <basketasl@hotmail.com>
2019-10-30 16:39:31 +0100

    albert commit

--- MariosPizza/src/mariospizza/MariosPizza.java
+++ MariosPizza/src/mariospizza/MariosPizza.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
 
 //vi har ikke kunne finde ud af at lave boolean afhentet false inde i et dokument om til true, så vi har ikke kunne lave en historie af bestillinger.
 //vi har ikke nået at lave statistik, hvilket også havde været svært at lave uden en historik.
-//når man trykker aktive bestillinger fårn 
+//når man trykker aktive bestillinger får man bare alle bestillinger der nogensinde er skrevet ind i programmet!...
 
 public class MariosPizza
 {
